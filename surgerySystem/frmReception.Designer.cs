abfcb949d9ba96152e63a4e054d961718f993aef
Elliot Roberts <310201@gllm.ac.uk>
2020-03-16 18:31:04 +0000

    Added ability to add a new patient

--- surgerySystem/frmReception.Designer.cs
+++ surgerySystem/frmReception.Designer.cs
@@ -29,32 +29,197 @@
         private void InitializeComponent()
         {
             this.btnLogout = new System.Windows.Forms.Button();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPCode = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtDOB = new System.Windows.Forms.MaskedTextBox();
             this.SuspendLayout();
             // 
             // btnLogout
             // 
-            this.btnLogout.Location = new System.Drawing.Point(24, 372);
+            this.btnLogout.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(803, 490);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(132, 66);
+            this.btnLogout.Size = new System.Drawing.Size(119, 48);
             this.btnLogout.TabIndex = 1;
             this.btnLogout.Text = "Log Out";
             this.btnLogout.UseVisualStyleBackColor = true;
             this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
+            // txtFirst
+            // 
+            this.txtFirst.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirst.Location = new System.Drawing.Point(366, 54);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(298, 43);
+            this.txtFirst.TabIndex = 2;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurname.Location = new System.Drawing.Point(366, 123);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(298, 43);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(366, 192);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(301, 43);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // txtPCode
+            // 
+            this.txtPCode.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPCode.Location = new System.Drawing.Point(366, 261);
+            this.txtPCode.MaxLength = 8;
+            this.txtPCode.Name = "txtPCode";
+            this.txtPCode.Size = new System.Drawing.Size(301, 43);
+            this.txtPCode.TabIndex = 5;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(366, 330);
+            this.txtPhone.MaxLength = 12;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(301, 43);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 41);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 41);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Surname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(84, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 41);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gadugi", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(84, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 41);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Post Code:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Gadugi", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(84, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(252, 41);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Phone Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Gadugi", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(84, 397);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(215, 41);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Date of Birth:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(415, 470);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(193, 58);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Add Patient";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Font = new System.Drawing.Font("Gadugi", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDOB.Location = new System.Drawing.Point(366, 399);
+            this.txtDOB.Mask = "00/00/0000";
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(301, 43);
+            this.txtDOB.TabIndex = 15;
+            this.txtDOB.ValidatingType = typeof(System.DateTime);
+            // 
             // frmReception
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(934, 550);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtPCode);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtFirst);
             this.Controls.Add(this.btnLogout);
             this.Name = "frmReception";
-            this.Text = "frmReception";
+            this.Text = "Receptionist";
+            this.Load += new System.EventHandler(this.frmReception_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPCode;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.MaskedTextBox txtDOB;
     }
 }
--- surgerySystem/frmReception.cs
+++ surgerySystem/frmReception.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Collections;
 
 namespace surgerySystem
 {
     public partial class frmReception : Form
     {
+        DataSet dsDatabase;
+        System.Data.SqlClient.SqlDataAdapter daGetData;
+        public int whichRec = 0;
         public frmReception()
         {
             InitializeComponent();
@@ -22,6 +28,55 @@ namespace surgerySystem
             this.Hide();
             frmLogin login = new frmLogin();
             login.Show();
+        }
+
+        private void MoveRecords()
+        {
+            DataRow OneRecord = dsDatabase.Tables["tblPatient"].Rows[whichRec];  
+        }
+
+        private void frmReception_Load(object sender, EventArgs e)
+        {
+            string myCon = ConfigurationManager.ConnectionStrings["SurgeryConnString"].ConnectionString;
+            SqlConnection mySQLCon = new SqlConnection(myCon);
+            mySQLCon.Open();
+
+            dsDatabase = new DataSet();
+
+            String sqlGetWhat;
+            sqlGetWhat = "SELECT * From tblPatient";//Selects all records
+
+            daGetData = new System.Data.SqlClient.SqlDataAdapter(sqlGetWhat, myCon);
+
+            daGetData.Fill(dsDatabase, "tblPatient");
+            MoveRecords();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtFirst.Text == "" || txtSurname.Text == "" || txtAddress.Text == "" || txtPCode.Text == "" || txtPhone.Text == "" || txtDOB.Text == "")
+            {
+                MessageBox.Show("Please ensure all fields are filled in before saving");//Prevents user from saving blank data
+            }
+            else
+            {
+                DataRow OneRecord = dsDatabase.Tables["tblPatient"].NewRow();
+
+                OneRecord[1] = txtFirst.Text;//Adds new record, saves data using data from textboxes
+                OneRecord[2] = txtSurname.Text;
+                OneRecord[3] = txtAddress.Text;
+                OneRecord[4] = txtPCode.Text;
+                OneRecord[5] = txtPhone.Text;
+                OneRecord[6] = txtDOB.Text;
+
+                dsDatabase.Tables["tblPatient"].Rows.Add(OneRecord);
+
+                System.Data.SqlClient.SqlCommandBuilder myUpdateDB;
+                myUpdateDB = new System.Data.SqlClient.SqlCommandBuilder(daGetData);
+                myUpdateDB.DataAdapter.Update(dsDatabase.Tables["tblPatient"]);
+
+                MoveRecords();
+            }
         }
     }
 }
